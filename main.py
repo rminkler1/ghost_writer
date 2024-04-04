c6d69ad8ed5389cf8c18fba834a378f36b2c37c0
robertminkler <robertminkler@gmail.com>
2024-04-03 21:41:13 -0700

    Scrolled text box

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-from tkinter import *
+import tkinter as tk
+from tkinter import scrolledtext
 
 # Constants
 APP_NAME = "Ghost Writer"
 FONT = ("Arial", 30, "bold")
 
 # Window config
-window = Tk()
+window = tk.Tk()
 window.title(APP_NAME)
 window.minsize(width=1000, height=600)
 window.config(padx=20, pady=20)
 
-# User input
-text_box = Text(height=5, width=50, font=FONT, wrap=WORD)
+# User inputscrolledtext.ScrolledText
+text_box = scrolledtext.ScrolledText(height=15, width=50, font=FONT, wrap=tk.WORD)
 text_box.focus()
 text_box.config(pady=20, padx=20)
 text_box.pack(pady=30)
+
 
 window.mainloop()
